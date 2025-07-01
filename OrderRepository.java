15409fc74802fd2d3df33b90ad1607e1e015f30a
Ravina Mahariya <ravinamahariyamahariya@gmail.com>
2025-07-01 11:58:30 +0530

    Delete OrderRepository.java

--- OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package Ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Ecommerce.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
